c8de66868a5df203f480f7f62509ce5a18cb3429
Doug-miguel <doug_miguel@outlook.com>
2022-08-11 21:00:14 -0300

    loginCreate

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
+import { USER_POST } from "../../Api/api";
+import { UserContext } from "../../Context/UserContext";
+import useForm from "../../Hooks/useForm";
+import Button from "../Form/Button";
+import Input from "../Form/Input";
 
 const LoginCreate = () => {
-  return <div>Login Criar</div>;
+  const username = useForm();
+  const password = useForm("password");
+  const email = useForm("email");
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      password: password.value,
+      email: email.value,
+    });
+    const response = await fetch(url, options);
+    if (response.ok) userLogin(username.value, password.value);
+  }
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
