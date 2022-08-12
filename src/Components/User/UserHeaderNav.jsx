68278b6937e25837148d2291c93d5faa1a01bd40
Doug-miguel <doug_miguel@outlook.com>
2022-08-12 20:11:47 -0300

    PhotoPost

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { Sair } from "../../Assets/sair";
 import { Feed } from "../../Assets/Feed";
 import { Adicionar } from "../../Assets/Adicionar";
@@ -7,31 +7,53 @@ import { Estatisticas } from "../../Assets/Estatisticas";
 import { UserContext } from "../../Context/UserContext";
 
 import style from "./UserHeaderNav.module.css";
+import useMedia from "../../Hooks/useMedia";
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
-
+  const [mobileMenu, setMobileMenu] = React.useState(false);
   const { userLogout } = React.useContext(UserContext);
 
+  const mobile = useMedia("(max-width: 40rem");
+
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
-    <nav className={style.nav}>
-      <NavLink to="/conta" end>
-        <Feed />
-        {mobile && "Minhas Fotos"}
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <Estatisticas />
-        {mobile && "Estatísticas"}
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <Adicionar />
-        {mobile && "Adicionar Fotos"}
-      </NavLink>
-      <button onClick={userLogout}>
-        <Sair />
-        {mobile && "Sair"}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          area-aria-label="Menu"
+          className={`${style.mobileButton} ${
+            mobileMenu && style.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+      <nav
+        className={`${mobile ? style.navMobile : style.nav} ${
+          mobileMenu && style.navMobileActive
+        }`}
+      >
+        <NavLink to="/conta" end>
+          <Feed />
+          {mobile && "Minhas Fotos"}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas />
+          {mobile && "Estatísticas"}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <Adicionar />
+          {mobile && "Adicionar Fotos"}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Sair />
+          {mobile && "Sair"}
+        </button>
+      </nav>
+    </>
   );
 };
 
--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+import Input from "../../Components/Form/Input";
+import Button from "../../Components/Form/Button";
+
+import style from "./UserPhotoPost.module.css";
+
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+
+import { PHOTO_POST } from "../../Api/api";
+import Error from "../../Helper/Error";
+import { useNavigate } from "react-router-dom";
 
 const UserPhotoPost = () => {
-  return (
-    <div>
-      UserPhotoPost
-    </div>
-  )
-}
+  const name = useForm();
+  const idade = useForm("number");
+  const peso = useForm("number");
+  const [img, setImg] = React.useState({});
+  const navigate = useNavigate();
 
-export default UserPhotoPost
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    if (data) navigate("/conta");
+  }, [data, navigate]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("img", img.raw);
+    formData.append("nome", name.value);
+    formData.append("peso", peso.value);
+    formData.append("idade", idade.value);
+
+    const token = window.localStorage.getItem("token");
+    const { url, options } = PHOTO_POST(formData, token);
+    if (token) request(url, options);
+  }
+
+  function handleImgChange({ target }) {
+    setImg({
+      preview: URL.createObjectURL(target.files[0]),
+      raw: target.files[0],
+    });
+  }
+
+  return (
+    <section className={`${style.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="name" {...name} />
+        <Input label="Idade" type="number" name="idade" {...idade} />
+        <Input label="Peso" type="number" name="peso" {...peso} />
+        <input
+          className={style.file}
+          type="file"
+          name="img"
+          id="img"
+          onChange={handleImgChange}
+        />
+        {loading ? (
+          <Button disabled>Enviando...</Button>
+        ) : (
+          <Button>Enviar</Button>
+        )}
+        <Error error={error} />
+      </form>
+      <div>
+        {img.preview && (
+          <div
+            className={style.preview}
+            style={{ backgroundImage: `url(${img.preview})` }}
+          ></div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default UserPhotoPost;
