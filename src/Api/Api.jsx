b21e3977634730ad1600a068538683b7679d2853
Doug-miguel <doug_miguel@outlook.com>
2022-08-14 16:09:22 -0300

    password

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -89,7 +89,6 @@ export function PHOTOS_GET_ONE(id) {
   };
 }
 
-
 export function COMMENT_POST(id, token, body) {
   return {
     url: `${API_URL}/api/comment/${id}`,
@@ -116,4 +115,40 @@ export function PHOTO_DELETE(id, token) {
   };
 }
 
+export function PASSWORD_LOST(body) {
+  return {
+    url: `${API_URL}/api/password/lost`,
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
 
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function STATS_GET() {
+  return {
+    url: API_URL + '/api/stats',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+    },
+  };
+}
--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+import Input from "../Form/Input";
+import Button from "../Form/Button";
+
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+
+import { PASSWORD_LOST } from "../../Api/api";
+
+import Error from "../../Helper/Error";
+
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const login = useForm();
+  const { request, data, loading, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate()) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace("perdeu", "resetar"),
+      });
+      await request(url, options);
+    }
+  }
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha ?</h1>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="Login" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar Email</Button>
+          )}
+        </form>
+      )}
+
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
